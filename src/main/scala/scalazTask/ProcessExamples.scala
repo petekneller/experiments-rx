56e0d5534d8c55f7d1800b01e221fd369662e9af
petekneller <mrkneller@hotmail.com>
2015-03-08 21:43:24 +0000

    some beginning examples of scalaz-streams; exploring the Process0 concept

---
+++ src/main/scala/scalazTask/ProcessExamples.scala
@@ -0,0 +1,27 @@
+package scalazTask
+
+import scalaz.{\/-, \/, Catchable}
+import scalaz.concurrent._
+import scalaz.stream.{Process0, Process}
+import scalaz.std.list._
+
+object ProcessNothing extends App {
+
+//  val a: Process[Nothing, Int] = Process.emitAll(Seq(1, 2, 3))
+  val a: Process[Nothing, Int] = Process.emitAll(Seq(1, 2, 3)) ++ Process.emitAll(Seq(4, 5, 6))
+  println(s"a = $a")
+  // println(a.run) // doesn't compile, no Catchable instance
+
+  val c: Process[Task, Int] = a
+  println(s"c.runLast = ${c.runLast}")
+  println(s"c.runLast.run = ${c.runLast.run}")
+  println(s"c.runLog.run = ${c.runLog.run}")
+
+  // List isn't supported out of the box (no Catchable) but it doesn't seem too hard to do
+  val b: Process[List, Int] = a
+  implicit val listCatchable = new Catchable[List] {
+    override def attempt[A](f: List[A]): List[Throwable \/ A] = f map { a => \/-(a) }
+    override def fail[A](err: Throwable): List[A] = throw err
+  }
+  println(s"b.runLog = ${b.runLog}")
+}
