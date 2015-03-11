2342643370cf6d9e7bc5b18360edec1f950d6d77
Pete Kneller <Peter.Kneller@barclayswealth.com>
2015-03-11 14:21:17 +0000

    some examples of the combinators/constructors available

--- src/main/scala/scalazTask/ProcessExamples.scala
+++ src/main/scala/scalazTask/ProcessExamples.scala
@@ -1,7 +1,8 @@
 package scalazTask
 
+import scalaz.Id
 import scalaz.stream.Process.Halt
-import scalaz.{Id, \/-, \/, Catchable}
+import scalaz._
 import scalaz.concurrent._
 import scalaz.stream.{Cause, Process0, Process}
 import scalaz.std.list._
@@ -21,18 +22,12 @@ object ProcessNothing extends App {
 
   // List isn't supported out of the box (no Catchable) but it doesn't seem too hard to do
   val b: Process[List, Int] = a
-  implicit val listCatchable = new Catchable[List] {
-    override def attempt[A](f: List[A]): List[Throwable \/ A] = f map { a => \/-(a) }
-    override def fail[A](err: Throwable): List[A] = throw err
-  }
+  implicit val listCatchable = Util.listCatchable
   println(s"b.runLog = ${b.runLog}")
 
   // Id monad
   val d: Process[Id, Int] = a
-  implicit val idCatchable = new Catchable[Id] {
-    override def attempt[A](a: Id[A]): Id[Throwable \/ A] = id.point(\/-(a))
-    override def fail[A](err: Throwable): Id[A] = throw err
-  }
+  implicit val idCatchable = Util.idCatchable
   println(s"d.runLog = ${d.runLog}")
 }
 
@@ -66,7 +61,61 @@ object ProcessStructure extends App {
   println(c)
   println(c.runLog.run)
 
-  // do some println's inside the stream to see how the computation progresses
+  val d = c.onHalt{ cause => Process.emit("boo!") }
+  println(d)
+  println(d.runLog.run)
+}
 
+object Combinators extends App {
+
+  val b = Process(1) ++ Process(2) ++ Process(3)
+  println(b.map(_+1))
+  println(b.flatMap(m => Process.emit(m+1)))
+
+  // Note how the below looks exactly like the above - the single process containing Seq(1, 2, 3) looks like it gets
+  // unravelled during the flatMap to a Process of Seq(1) and a Process of the rest
+  val a = Process(1, 2, 3)
+  println(a.map(_+1))
+  println(a.flatMap(m => Process.emit(m+1)))
+
+  val c: Process[Task, Int] = Process.emit(1).repeat // look at the impl of this - its wild!
+  println(c)
+  println(c.take(10).runLog.run)
+
+  // this is an interesting one - shorthand for evaluating a single effect
+  // - see my example above for how I read from a list, then compare to the impl of 'eval'
+  val d = Process.eval(Task.delay{ 2 })
+  println(d.runLog.run)
+}
+
+object ChangingMonadicContext extends App {
+
+  implicit val idCatchable = Util.idCatchable
+  implicit val listCatchable = Util.listCatchable
+  
+  val a: Process[Id, Int] = Process(1, 2, 3)
+  println(a)
+  println(a.runLog)
+  val b = a.translate(new NaturalTransformation[Id, List] { def apply[A](a: Id[A]): List[A] = List(a) })
+  println(b)
+  println(b.runLog)
+  
+}
+
+object Program1 extends App {
   // how does |> work for Process1's?
+}
+
+object Util {
+
+  val idCatchable = new Catchable[Id] {
+    override def attempt[A](a: Id[A]): Id[Throwable \/ A] = id.point(\/-(a))
+    override def fail[A](err: Throwable): Id[A] = throw err
+  }
+
+  val listCatchable = new Catchable[List] {
+    override def attempt[A](f: List[A]): List[Throwable \/ A] = f map { a => \/-(a) }
+    override def fail[A](err: Throwable): List[A] = throw err
+  }
+
 }
